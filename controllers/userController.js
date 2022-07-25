82d548f1c217dc19c996e49b091368dbd0de3b5d
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-07-25 08:59:22 +0800

    fix logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,15 @@ const Exercise = require('../model/Exercise')
 
 exports.createUser = function (req,res) {
   
-  User.create({ username: req.body.username })
-    .then(doc => { res.status(201).json({ _id: doc._id , username: doc.username }) })
+  const { username } = req.body
+  
+  User.create({ username })
+    .then(({_id , username}) => { 
+      res.status(201).json({ 
+        username, 
+        _id 
+      }) 
+    })
     .catch(err => console.log(err))
 
 }
@@ -17,17 +24,72 @@ exports.createExercises = function (req,res) {
 
   User.findOne({ _id: id })
     .then(doc => {
-      
       Exercise.create({ user_id: mongoose.Types.ObjectId(doc._id),username: doc.username , description : description , duration : duration, date: date })
         .then(doc => {
-          res.status(201).json({ _id: doc.user_id , username: doc.username , description : doc.description , duration : doc.duration, date: doc.date })
+          let date = new Date(doc.date)
+          date = date.toDateString()
+          res.status(201).json({ 
+            username: doc.username,
+            description : doc.description,
+            duration : doc.duration,
+            date: date,
+            _id: doc.user_id
+          })
         })
         .catch(err => console.log(err))
 
     })
-
-  //res.json({ "_id": id,"date": date,"duration": parseInt(duration),"description": description })
-  //complete json response
-  // res.json({ "_id": id,"username": username ,"date": date,"duration": parseInt(duration),"description": description })
-  
+ 
 }
+
+exports.exerciseLogs = function (req,res) {
+  
+  const id = req.params.id
+  
+  // Exercise.find({ user_id: id})
+  //   .then(doc => {
+  //     res.status(200).json(doc)
+  //   }).catch(err => console.log(err))
+
+  Exercise.find({ user_id: id})
+    .sort({date: 1})
+    .select({
+      username: true,
+      user_id: true,
+      description: true,
+      duration: true,
+      date:true
+    })
+    .exec()
+    .then(doc => {
+      let jsonData = {
+        username: doc[0].username,
+        count: doc.length,
+        _id: doc[0].user_id,
+        logs: []  
+      }
+      for (let i = 0 ; i < doc.length ; i++) {
+        let date = new Date(doc[i].date).toDateString()
+        jsonData.logs.push({
+          description: doc[i].description,
+          duration: doc[i].duration,
+          date: date
+        })
+      }
+
+      res.json(jsonData)
+    })
+    .catch(err => console.log(err))
+}
+
+//logs
+// {
+//   username: "fcc_test",
+//   count: 1,
+//   _id: "5fb5853f734231456ccb3b05",
+//   log: [{
+//     description: "test",
+//     duration: 60,
+//     date: "Mon Jan 01 1990",
+//   }]
+// }
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/userController');
 
 router.post('/users', userController.createUser)
 router.post('/users/:id/exercises' , userController.createExercises)
+router.get('/users/:id/logs' , userController.exerciseLogs)
 
 module.exports = router
